f2438b0acd64770adda8002ae243b0ac006af3de
lz29lz29 <lz29@njit.edu>
2022-04-25 21:07:10 -0400

    Add logging configuration

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from app.context_processors import utility_text_processors
 from app.simple_pages import simple_pages
 from app.songs import songs
 from app.auth import auth
+from app.logging_config import log_con
 from app.exceptions import http_exceptions
 from app.db.models import User
 from app.db import db
@@ -27,7 +28,15 @@ def page_not_found(e):
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+
+    if os.environ.get("FLASK_ENV") == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif os.environ.get("FLASK_ENV") == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif os.environ.get("FLASK_ENV") == "testing":
+        app.config.from_object("app.config.TestingConfig")
+
+
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
     csrf = CSRFProtect(app)
@@ -35,14 +44,13 @@ def create_app():
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
     app.register_blueprint(songs)
+    app.register_blueprint(log_con)
 
     app.context_processor(utility_text_processors)
-    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
+
     app.register_error_handler(404, page_not_found)
     # app.add_url_rule("/", endpoint="index")
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
     db.init_app(app)
     # add command function to cli commands
